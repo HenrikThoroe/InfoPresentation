53a8240a213ff1cc0ff8b27266dcfed481a31329
Henrik <henrik@thoroee.com>
2021-04-28 14:30:28 +0200

    Add visual array comp

---
+++ presentation/src/components/content/DataArray.tsx
@@ -0,0 +1,39 @@
+import Text from "./Text"
+import csx from "classnames"
+import styles from "./DataArray.module.css"
+
+export interface Props {
+    numbers: number[]
+    highlighted?: [number, number]
+    active?: number
+}
+
+export default function DataArray(props: Props) {
+    const isInRange = (index: number) => {
+        if (props.highlighted) {
+            return index >= props.highlighted[0] && index < props.highlighted[1]
+        }
+
+        return false
+    }
+
+    const createItems = () => {
+        return props.numbers.map((value, index) => {
+            return (
+                <li
+                    key={index}
+                    className={csx({
+                        [styles.active]: index === props.active,
+                        [styles.highlighted]: isInRange(index)
+                    })}
+                >
+                    <Text weight="bold" size="l">
+                        {value.toFixed(0)}
+                    </Text>
+                </li>
+            )
+        })
+    }
+
+    return <ul className={styles.dataArray}>{createItems()}</ul>
+}
