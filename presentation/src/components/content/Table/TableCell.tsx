59222d9771a7bbd1bd43cfbd4da102c5fdbe125e
Henrik <henrik@thoroee.com>
2021-05-04 20:11:48 +0200

    Allow centering of table cell content

--- presentation/src/components/content/Table/TableCell.tsx
+++ presentation/src/components/content/Table/TableCell.tsx
@@ -5,14 +5,27 @@ import csx from "classnames"
 
 export interface Props {
     children?: React.ReactNode
+    centered?: boolean
 }
 
 export default function TableCell(props: Props) {
     const isHeader = useContext(HeaderContext)
 
     if (isHeader) {
-        return <th className={csx(styles.tableCell, styles.tableHeaderCell)}>{props.children}</th>
+        return (
+            <th
+                className={csx(styles.tableCell, styles.tableHeaderCell, {
+                    [styles.centered]: props.centered
+                })}
+            >
+                {props.children}
+            </th>
+        )
     }
 
-    return <td className={styles.tableCell}>{props.children}</td>
+    return (
+        <td className={csx(styles.tableCell, { [styles.centered]: props.centered })}>
+            {props.children}
+        </td>
+    )
 }
