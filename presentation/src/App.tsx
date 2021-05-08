815ce223ae8384f4ac7fdc0c3b33608b2802ea60
Henrik <henrik@thoroee.com>
2021-05-08 15:45:33 +0200

    Show one of the views

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Benchmarks from "./views/Benchmarks"
+import Comparison from "./views/Comparison"
+import HowToRate from "./views/HowToRate"
+import Quote from "./views/Quote"
+import RadixSort from "./views/RadixSort"
+import Search from "./views/Search"
+import WhySorting from "./views/WhySorting"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Benchmarks />
 }
 
-export default App;
+export default App
