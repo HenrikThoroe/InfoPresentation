c2ea46b772b7c5bf84415154f757beb2b9fd3207
Henrik <henrik@thoroee.com>
2021-05-09 20:56:48 +0200

    Add radix and quick sort algorithms

---
+++ benchmarks/app/include/model/quick_sort.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <inttypes.h>
+#include <vector>
+
+namespace model {
+
+    /**
+     * Quick Sort is a comparison based, unstable, in-place sorting algorithm 
+     * which operates in O(n logn).
+     * 
+     * @param data An unsorted array of unsigned 64 bit numbers
+     */
+    void quick_sort(std::vector<uint64_t>& data);
+    
+}
