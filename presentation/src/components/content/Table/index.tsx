245ba295006689e7ed44ae811202f68fb1afb9bd
Henrik <henrik@thoroee.com>
2021-05-10 15:54:29 +0200

    Allow to set overall font size

--- presentation/src/components/content/Table/index.tsx
+++ presentation/src/components/content/Table/index.tsx
@@ -2,8 +2,13 @@ import styles from "./index.module.css"
 
 export interface Props {
     children?: React.ReactNode
+    fontSize?: string
 }
 
 export default function Table(props: Props) {
-    return <table className={styles.table}>{props.children}</table>
+    return (
+        <table className={styles.table} style={{ fontSize: props.fontSize }}>
+            {props.children}
+        </table>
+    )
 }
