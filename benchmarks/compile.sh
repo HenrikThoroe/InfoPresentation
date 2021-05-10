ebd08aa1876bd3cb5f837616b4cb588391845961
Henrik <henrik@thoroee.com>
2021-05-10 15:43:32 +0200

    Add extra compiler flags

--- benchmarks/compile.sh
+++ benchmarks/compile.sh
@@ -2,4 +2,4 @@
 mkdir -p dist/
 
 # Compile source files
-g++ -O3 -flto -pthread -Wall -std=c++17 -o dist/benchmarks.out -I app/include/libraries -I app/include/model -I app/include/util $(find ./app/src -type f -name "*.cpp")
+g++ -O3 -fsanitize=address -flto -pthread -fext-numeric-literals -Wall -std=c++17 -o dist/benchmarks.out -I app/include/libraries -I app/include/model -I app/include/util $(find ./app/src -type f -name "*.cpp")
