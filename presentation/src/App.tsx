bd3aa8dea3a3380bee18957082551d2af467bef8
Henrik <henrik@thoroee.com>
2021-05-12 12:22:57 +0200

    Add functionality to iterate views by keyboard

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,14 +1,76 @@
-import React from "react"
+import { useEffect, useState } from "react"
 import Benchmarks from "./views/Benchmarks"
 import Comparison from "./views/Comparison"
+import DatabaseRequirements from "./views/DatabseRequirements"
 import HowToRate from "./views/HowToRate"
 import Quote from "./views/Quote"
 import RadixSort from "./views/RadixSort"
 import Search from "./views/Search"
 import WhySorting from "./views/WhySorting"
 
+type View = "why" | "search" | "how" | "radix" | "comp" | "bench" | "dbreq" | "quote"
+
+const order: View[] = ["why", "search", "how", "radix", "comp", "bench", "dbreq", "quote"]
+
 function App() {
-    return <Benchmarks />
+    const [view, setView] = useState<View>("why")
+
+    const parseView = () => {
+        switch (view) {
+            case "why":
+                return <WhySorting />
+            case "search":
+                return <Search />
+            case "how":
+                return <HowToRate />
+            case "radix":
+                return <RadixSort />
+            case "comp":
+                return <Comparison />
+            case "bench":
+                return <Benchmarks />
+            case "dbreq":
+                return <DatabaseRequirements />
+            case "quote":
+                return <Quote />
+
+            default:
+                return <h1>Cannot Parse View</h1>
+        }
+    }
+
+    const next = () => {
+        const index = order.findIndex(value => value === view)
+
+        if (index < order.length - 1) {
+            setView(order[index + 1])
+        }
+    }
+
+    const prev = () => {
+        const index = order.findIndex(value => value === view)
+
+        if (index > 0) {
+            setView(order[index - 1])
+        }
+    }
+
+    const handleKeyEvent = (e: KeyboardEvent) => {
+        if (e.code === "ArrowRight") {
+            next()
+        } else if (e.code === "ArrowLeft") {
+            prev()
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", handleKeyEvent)
+        return () => {
+            document.removeEventListener("keydown", handleKeyEvent)
+        }
+    }, [view])
+
+    return parseView()
 }
 
 export default App
