80d5b841f0262ed9f3b1c1e4afd9387623e19e8b
Henrik <henrik@thoroee.com>
2021-05-13 22:16:49 +0200

    Make rows highlightable on alt-click

--- presentation/src/components/content/Table/TableRow.tsx
+++ presentation/src/components/content/Table/TableRow.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useState, MouseEvent } from "react"
 import HeaderContext from "./HeaderContext"
 import styles from "./index.module.css"
 import csx from "classnames"
@@ -9,9 +9,23 @@ export interface Props {
 
 export default function TableRow(props: Props) {
     const isHeader = useContext(HeaderContext)
+    const [isHighlighted, setIsHighlighted] = useState(false)
+
+    const trigger = (e: MouseEvent<HTMLTableRowElement, any>) => {
+        if (e.altKey) {
+            e.preventDefault()
+            setIsHighlighted(!isHighlighted)
+        }
+    }
 
     return (
-        <tr className={csx(styles.tableRow, { [styles.tableHeader]: isHeader })}>
+        <tr
+            className={csx(styles.tableRow, {
+                [styles.tableHeader]: isHeader,
+                [styles.highlighted]: isHighlighted
+            })}
+            onClick={trigger}
+        >
             {props.children}
         </tr>
     )
