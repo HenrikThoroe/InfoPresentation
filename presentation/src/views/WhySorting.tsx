7a0253648c2e0d193f3afcf8d75199601787692a
Henrik <henrik@thoroee.com>
2021-05-13 22:03:20 +0200

    Rewrite UI text less casual

--- presentation/src/views/WhySorting.tsx
+++ presentation/src/views/WhySorting.tsx
@@ -8,7 +8,7 @@ export default function WhySorting() {
         <Container overflow="hidden" width="100vw" height="100vh">
             <VStack alignment="center" horizontalAlignment="center" spacing="10rem">
                 <Text size="xxxl" weight="bold">
-                    Warum muss immer alles sortiert werden?
+                    Warum ist Sortieren so wichtig?
                 </Text>
 
                 <VStack spacing="2rem" alignment="center" padding="0px 20%" shrinkHeight>
@@ -16,14 +16,16 @@ export default function WhySorting() {
                         <Text size="xxl" weight="heavy">
                             -
                         </Text>
-                        <Text size="xxl">Weil wir, die Benutzer, das so wollen!</Text>
+                        <Text size="xxl">
+                            Weil Sortieren eine Aufgabe ist, die der Benutzer erledigen will
+                        </Text>
                     </HStack>
                     <HStack spacing="1rem" shrinkHeight>
                         <Text size="xxl" weight="heavy">
                             -
                         </Text>
                         <Text size="xxl">
-                            Um effizienter zu sein, bzw. Aufgaben überhaupt erledigen zu können.
+                            Um effizienter zu sein, bzw. Aufgaben überhaupt erledigen zu können
                         </Text>
                     </HStack>
                 </VStack>
